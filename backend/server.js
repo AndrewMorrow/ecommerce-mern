dd2bca70d66ba1896761e03755a7341be42489a6
AndrewMorrow <beardedmongo@gmail.com>
2021-02-20 19:42:27 -0600

    connects mongodb atlas and compass

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-const express = require("express");
+import express from "express";
 const PORT = process.env.PORT || 5000;
-const products = require("./data/products");
+import products from "./data/products.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -19,5 +22,7 @@ app.get("/api/products/:id", (req, res) => {
 
 app.listen(
     PORT,
-    console.log(`Server running on port: http://localhost:${PORT}`)
+    console.log(
+        `Server running in ${process.env.NODE_ENV} on port: http://localhost:${PORT}`
+    )
 );
